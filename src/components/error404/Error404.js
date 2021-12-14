4449f71ddf1011bd1bc44eb1a66838e3a246f003
diegojoel98 <58920143+diegojoel98@users.noreply.github.com>
2021-12-14 12:28:38 -0600

    Install gh-pages and deploy the project

--- src/components/error404/Error404.js
+++ src/components/error404/Error404.js
@@ -7,7 +7,7 @@ export default function Error404() {
                 <div className="col-md-12" style={{ marginTop: "35vh" }}>
                     <small>Error 404 - Page not found</small>
                     <h2>Sorry, the page you are looking does not exist 😥</h2>
-                    <p>You can go back to the <Link to="/"> <u>index</u> </Link> and continue browsing </p>
+                    <p>You can go back to the <Link to="/covid-data-webapp"> <u>index</u> </Link> and continue browsing </p>
                 </div>
             </div>
         </div>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,20 +7,20 @@ export default function Navbar() {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#C8E6C9" }}>
-            <Link to="/" className='navbar-brand'> Covid-19 Webapp  <i className="fas fa-virus"></i></Link>
+            <Link to="/covid-data-webapp" className='navbar-brand'> Covid-19 Webapp  <i className="fas fa-virus"></i></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <Link to="/" className='nav-link'>Home <span className="sr-only">(current)</span></Link>
+                        <Link to="/covid-data-webapp" className='nav-link'>Home <span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/#covid-world" className='nav-link'>Covid in the World</Link>
+                        <Link to="/covid-data-webapp#covid-world" className='nav-link'>Covid in the World</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/#daily-summary" className='nav-link'>Daily Summary</Link>
+                        <Link to="/covid-data-webapp#daily-summary" className='nav-link'>Daily Summary</Link>
                     </li>
                 </ul>
             </div>
